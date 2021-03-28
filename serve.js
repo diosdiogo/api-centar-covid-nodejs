daf064998147368e85a17cb63c733acfce98097b
@diogoCesar <diogo.andrade@plss.com.br>
2021-03-27 23:04:09 -0300

    configuração heroku

--- serve.js
+++ serve.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.port || 43760;
+const port = process.env.port || 3000;
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port, "0.0.0.0", function() {
+    console.log(port);
+});
